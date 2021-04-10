acf1eb54ab0288b42440faaa187a47920db03429
dcfernandez1023 <54515508+dcfernandez1023@users.noreply.github.com>
2021-04-10 00:50:50 -0700

    init firebase

---
+++ training-diary-frontend/src/views/App.js
@@ -0,0 +1,16 @@
+/* react library imports */
+import React, { useState, useEffect } from 'react';
+
+/* style imports */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <Container fluid>
+      <h1> TrainingDiary </h1>
+    </Container>
+  );
+}
+
+export default App;
